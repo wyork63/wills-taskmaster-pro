373a66d01321ec515f7e8aaf1cb459390a6b2933
wyork <w.york63@gmail.com>
2021-12-16 17:09:53 -0600

    added fonts and timeinterval

--- assets/js/script.js
+++ assets/js/script.js
@@ -283,6 +283,12 @@ var auditTask = function(taskEl) {
 };
 
 
+setInterval(function() {
+  $(".card .list-group-item").each(function(index, el) {
+    auditTask(el);
+  });
+}, (1000 * 60) * 30);
+
 // load tasks for the first time
 loadTasks();
 
